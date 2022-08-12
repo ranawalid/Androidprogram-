c5e14fc2fdbd91f96a26479cc89e8bdf0878d7e4
ranawalid <103863926+ranawalid@users.noreply.github.com>
2022-08-12 07:45:08 +0200

    version 2

--- src/MainFile.kt
+++ src/MainFile.kt
@@ -1,11 +1,11 @@
 fun main(){
-    val version = "version 1 "
+
       var number1 : Double =25.0
       var number2 : Byte =20
     var result = number1+number2
     println(result)
 
-
+// final 
 
 
 
